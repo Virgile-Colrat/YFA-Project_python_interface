a4102d789869b75fc5828d7b88280b18aac16ae5
virgile <virgile.colrat@utah.edu>
2020-05-20 15:27:26 -0600

    MFC documentation added

---
+++ Documentation/Gas_flow_controller_manual.md
@@ -0,0 +1,1 @@
+extensive serial communication detail: https://documents.alicat.com/manuals/Gas_Flow_Controller_Manual.pdf
--- Sources/SerialComOmeagaette.py
+++ Sources/SerialComOmeagaette.py
@@ -1,4 +1,4 @@
 import serial
 
 ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=10, rtscts=0, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS, xonxoff=False)
-ser.write(b'A\r')
+ser.write(b'E\r')
