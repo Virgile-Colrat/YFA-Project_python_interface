48bdf6862653ea34926156bd93c167bf3effc590
Virgile <virgile.colrat@gmail.com>
2020-05-21 18:30:19 +0200

    backup

--- Sources/SerialComOmeagaette.py
+++ Sources/SerialComOmeagaette.py
@@ -1,4 +1,14 @@
 import serial
-
+import time
 ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=10, rtscts=0, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS, xonxoff=False)
 ser.write(b'E\r')
+time.sleep(3)
+ser.write(b'E\r')
+time.sleep(1)
+ser.write(b'P\r')
+x=ser.read()
+"""print(ord(x))"""
+while x != 0:
+	print(ord(x))
+	x=ser.read()
+	
