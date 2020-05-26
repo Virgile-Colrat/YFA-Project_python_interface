ed036b98267001e6aeb3223863ee231edc800353
Virgile <virgile.colrat@utah.edu>
2020-05-26 14:56:14 -0600

    backup-small changes

--- Sources/SerialComOmeagaette.py
+++ Sources/SerialComOmeagaette.py
@@ -1,14 +1,18 @@
 import serial
+"""
 import time
-ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=10, rtscts=0, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS, xonxoff=False)
+ser = serial.Serial('COM17', 9600, timeout=10, rtscts=0, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS, xonxoff=False)
 ser.write(b'E\r')
 time.sleep(3)
 ser.write(b'E\r')
 time.sleep(1)
 ser.write(b'P\r')
 x=ser.read()
-"""print(ord(x))"""
+print(ord(x))
 while x != 0:
 	print(ord(x))
 	x=ser.read()
-	
+"""             # close port
+s = serial.Serial('COM4')
+res = s.read()
+print(res)
