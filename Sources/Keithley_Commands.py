64b08775796610a6c731b2d98432a0774e77745e
Virgile <virgile.colrat@utah.edu>
2020-06-03 08:21:01 -0600

    Keithley cmds added

---
+++ Sources/Keithley_Commands.py
@@ -0,0 +1,20 @@
+def ModifySweepList(level, range, delay, first, last):#A(level),(range),(delay),first(,last)
+def BiasOperation(level, range, delay):#B(level),(range),(delay)
+def Calibration(step, value):#C step (,value) X
+def Display(D):#DOX/D1,aaa ... aX/D2,aaa ... aX
+def SourceAndFunction(source, function):#F(source) ,(function)
+def OutputDataFormat(items, format, lines):#G(items) ,(format) ,(lines)
+def IEEEImmediateTrigger(H):#HOX
+def SelfTests(J):#JOX/J1X/J2X
+def EOIAndBusHoldOff(K):#K0/K1/K2/K3
+def Compliance(level, range):#L(level) ,(range)
+def SRQMaskAndSerialPollByte(mask, compliance):#M(mask),(compliance)
+def Operate(N):#N0/N1
+def OutputSense(O):#O0/01
+def Filter(P):#P1/P2/P3/P4/P5
+def CreateAppendSweepList(Q):#Q0/Q1/Q2/Q3/Q4/Q5/Q6/Q7/Q8/Q9/Q10/Q11
+def TriggerControl(R):#R0/R1
+def IntegrationTime(S):#S0/S1/S2/S3
+def TriggerConfiguration(origin, In, out, end):#T( origin) ,(in),( out),( end)
+def Status():
+def DefaultDelay():
