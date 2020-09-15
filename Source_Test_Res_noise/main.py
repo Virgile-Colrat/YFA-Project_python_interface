17098e6b56d0353e5686c5921e58ab70f76379e6
Virgile <virgile.colrat@utah.edu>
2020-09-15 17:16:24 +0200

    updated source for smu noise

--- Source_Test_Res_noise/main.py
+++ Source_Test_Res_noise/main.py
@@ -35,7 +35,7 @@ from alicat import FlowController
 
 filename="results_noise_caracterization_res"                                          #names the file in which the results are stored 
 rm = pyvisa.ResourceManager()
-polarisationVoltage=1                                
+polarisationVoltage=2.2                                
 numberMeasures=50
 ligne=["","","","","","","","",""]
 
